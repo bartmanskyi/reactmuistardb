db4b98b1775a6010e6ce189e717890b0601f174b
Volodymyr Bartmanskyi <bartmanskyi@gmail.com>
2019-07-10 11:15:49 +0300

    init commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+// const swapi = new SwapiService();
+
+// swapi.getAllPeople().then((people) => {
+// 	console.log(people);
+// });
